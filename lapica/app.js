fa5bb0e7efb6387e1121ceb00fd2548588fcc779
Moritz Gruber <Moritz@Moritzs-Air.fritz.box>
2016-04-17 23:41:44 +0200

    cleard all overhard created by node setup, just use need stuff for socket setup

--- lapica/app.js
+++ lapica/app.js
@@ -1,71 +1,21 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
-
-//adding dependecies for socket.io server connection here
+var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io').(http);
+var io = require('socket.io')(http);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
 
-//socket.io server listing on connections ...
-io.on('connect', function(socket){
-  console.log('A user has connected');
-  socket.on('disconnect', function(){
-    console.log('A user has disconnected');
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+  console.log('A new connection is now open');
+  socket.on('disconnect', function () {
+    console.log('A connection was closed');
   });
 });
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
-module.exports = app;
