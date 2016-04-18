20656d03032e1950524e78221596c502a6e1fd55
Moritz Gruber <Moritz@Moritzs-MacBook-Air.local>
2016-04-18 13:01:00 +0200

    now server is working with socket.io and you can test it with the web clint so the console should also log the messages

--- lapica/app.js
+++ lapica/app.js
@@ -2,14 +2,19 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+//runnig index.html as simple web client here ==> see on ip:3000
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+//get called if somebody connects via socket.io to our ip
 io.on('connection', function(socket){
+    //print out the chat message to the console and emit it to all clints
   socket.on('chat message', function(msg){
+    console.log('A message was transmitted: ' + msg );
     io.emit('chat message', msg);
   });
+    //showing when somebody opens socket.io connection or closes
   console.log('A new connection is now open');
   socket.on('disconnect', function () {
     console.log('A connection was closed');
