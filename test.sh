909f8362e7e12042b8fe53255513336ad01c98df
MoritzGruber <moritzgruber@yahoo.de>
2016-07-24 09:05:01 -0400

    removing detach option from docker exec because it runs asyc and then mocha ist installed intime when we want to use it ==> or next command dont wait

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 # Attatch to running node Container, install mocha, run mocha test
-docker exec -d serverfashionapphdmmwa_node_1 npm install mocha -g
+docker exec serverfashionapphdmmwa_node_1 npm install mocha -g
 docker exec serverfashionapphdmmwa_node_1 mocha
