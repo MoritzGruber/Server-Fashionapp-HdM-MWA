6c790bf6bb043dd97b18eea6a4b3be58dcc607c8
MoritzGruber <moritzgruber@yahoo.de>
2016-07-25 16:55:25 -0400

    refesh socket from callback to promise

--- lapica/app.js
+++ lapica/app.js
@@ -1,7 +1,14 @@
 // Main Node APP 
 // here all comes together 
 
+//external node modules
+var app = require('express')();
+var http = require('http').Server(app);
+
 //own logic modules
 var socketService = require('./socketService');
 
-
+//normal http server, we attach socket.io to this
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
--- lapica/debug.js
+++ lapica/debug.js
@@ -4,27 +4,18 @@
 //just fore debugging
 //overriding normal console log
 
-var pushNotification = require('./pushNotification');
-
-
 var DEBUG = true;
-var enablePushNotification = true;
 
-//make pushnotifications diabled, so we dont spam everyone while we are testing
-setTimeout(function(){
-  pushNotification.setPushEnabeld(enablePushNotification);
-},1000);
+logusers = function () {
+    if (DEBUG == true) {
+        setTimeout(function () {
+            console.log("Users Online: " + users_online_cache.length + " Users Offlien: " + users_offline_cache.length);
+        }, 1000);
+    }
+};
 
-var exports = module.exports = {};
-  exports.logusers = function (users_online_cache, users_offline_cache) {
-      if (DEBUG === true) {
-          setTimeout(function () {
-              console.log("Users Online: " + users_online_cache.length + " Users Offlien: " + users_offline_cache.length);
-          }, 1000);
-      }
-  };
-exports.log = function (msg) {
-      if (DEBUG === true) {
-          console.log(msg);
-      }
-  }
+log = function (msg) {
+    if (DEBUG == true) {
+        console.log(msg);
+    }
+};
