63b3e4d8d88b9951d43b20e8640ad917ae517403
MoritzGruber <moritzgruber@yahoo.de>
2016-11-19 12:24:08 +0100

    added more rest structure and newImage api

--- lapica/restAPI.js
+++ lapica/restAPI.js
@@ -8,6 +8,14 @@ var express    = require('express');        // call express
 var app = require('./main').app;        // define our app using express
 var bodyParser = require('body-parser');
 
+//load other modules
+var Promise = require('bluebird');
+var usersAsync = Promise.promisifyAll(require('./controllers/users'));
+var picturesAsync = Promise.promisifyAll(require('./controllers/pictures'));
+var pushNotification = require('./pushNotification');
+var debug = require('./debug');
+var sms = require('./smsService');
+
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,12 +26,55 @@ var port = process.env.PORT || 8080;        // set our port
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
+// startVerify
+router.post('/startVerify', function (req, res) {
+    res.json({ message: 'hooray! welcome to our startVerify!' });
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    console.log('asdf');
-    res.json({ message: 'hooray! welcome to our api!' });
 });
+//checkVerify
+router.post('/checkVerify', function (req, res) {
+    res.json({ message: 'hooray! welcome to our checkVerify!' });
+
+});
+//newImage
+router.post('/newImage', function (req, res) {
+    var data = req.body;
+    if (data.transmitternumber != null) {
+        //we got an image form a sender
+        usersAsync.getUserIdFromPhonenumberAsync(data.transmitternumber).then(function (userId) {
+            //we got the id of that sender
+            debug.log("data.transmitternumber = " + data.transmitternumber + " ,userid = " + userId + ' uploaded a new image');
+            return picturesAsync.createPictureAsync(data.imageData, userId, data.recipients);
+        }).then(function (resId) {
+            //send the sender(clint) a msg back, so he can add the correct server image id too
+            var outgoing_image = {};
+            outgoing_image._id = resId;
+            outgoing_image.imageData = data.imageData;
+            outgoing_image.transmitternumber = data.transmitternumber;
+            //give the sender back the server id"
+            res.json({ serverId: resId, localId: data.localImageId}); //resId == server id, localImageId == clint id to sender so he can assign the id
+            debug.log('Image succsesful created and server id send back. The resId after createPicture was == ' + resId);
+        }).then(function () {
+            //send push notification to other clients that are offline
+            //online clients will call a refresh to fetch the new image"
+            return pushNotification.sendPush(users_offline_cache, "You got a new Image");
+        }).catch(function (error) {
+            //something in
+            console.log('Creating Image Failed: ' + error);
+        });
+    }
+
+});
+//update
+router.get('/update', function(req, res) {
+    res.json({ message: 'hooray! welcome to our update!' });
+});
+//vote
+router.post('/vote', function (req, res) {
+    res.json({ message: 'hooray! welcome to our vote!'+ JSON.stringify(req.body)});
+
+});
+
 
 // more routes for our API will happen here
 
--- lapica/socketService.js
+++ lapica/socketService.js
@@ -149,16 +149,6 @@ io.on('connection', function (socket) {
             });
         }
     });
-    //add image
-    app.post('/addImage', function (req, res) {
-        // First read existing users.
-        fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-            data = JSON.parse( data );
-            data["user4"] = user["user4"];
-            console.log( data );
-            res.end( JSON.stringify(data));
-        });
-    });
 
 
     //refresh call
