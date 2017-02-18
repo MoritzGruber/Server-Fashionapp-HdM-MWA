619e0ebcc18e547b03296ddc9967465660c39473
Christoph <chrisi.muth@gmx.de>
2017-02-18 17:33:45 +0100

    updated image and vote controller

---
+++ lapica/controllers/image.js
@@ -0,0 +1,55 @@
+var Image = require('./../models/image');
+var debug = require('./../debug');
+
+module.exports = {
+    // create image
+    createImage: function (creator, product, source, callback) {
+        debug.log("createImage called");
+        var image = new Image({
+            creator: creator,
+            createDate: Date.now(),
+            active: true,
+            product: null,
+            source: source
+        });
+        image.save(function (err, res) {
+            callback(err, res._id);
+        });
+    },
+
+    // get all images
+    getImages: function (callback) {
+        debug.log("getImages called");
+        Image.find().select('_id').exec(function (err, res) {
+            var imageIds = [];
+            res.forEach(function (image) {
+                imageIds.push(image._id);
+            });
+            callback(err, imageIds);
+        });
+    },
+
+    // get single image
+    getImage: function (id, callback) {
+        debug.log("getImage called");
+        Image.findOne({_id: id}, function (err, res) {
+            callback(err, res);
+        });
+    },
+
+    // get user(creator) of picture
+    getUserOfImage: function (id, callback) {
+        debug.log("getUserOfImage called");
+        Image.findOne({_id: id}, function (err, res) {
+            callback(err, res.user);
+        });
+    },
+
+    // delete image
+    deleteImage: function (imageId, callback) {
+        debug.log("deleteImage called");
+        Image.remove({_id: id}, function (err, res) {
+            callback(err, res);
+        });
+    }
+};
---
+++ lapica/controllers/vote.js
@@ -0,0 +1,66 @@
+var Vote = require('./../models/vote');
+var debug = require('./../debug');
+
+module.exports = {
+    // create vote 
+    createVote: function (value, userId, imageId, callback) {
+        debug.log("createVote called");
+        var vote = new Vote({
+            value: value,
+            dateCreated: Date.now(),
+            user: userId,
+            image: imageId
+        });
+        vote.save(function (err, res) {
+            callback(err, res._id);
+        });
+    },
+
+    // get all votes
+    getVotes: function (callback) {
+        debug.log("getVotes called");
+        Vote.find(function (err, res) {
+            var voteIds = [];
+            res.forEach(function (vote) {
+                voteIds.push(vote._id);
+            });
+            callback(err, voteIds);
+        });
+    },
+
+    // get some spesific votes
+    getVotesOfImage: function (imageId, callback) {
+        debug.log("getVotesOfImage called");
+        Vote.find({image: imageId}, function (err, res) {
+            var voteIds = [];
+            res.forEach(function (vote) {
+                voteIds.push(vote._id);
+            });
+            callback(err, voteIds);
+        });
+    },
+
+    // get single vote
+    getVoteByUserAndImage: function (userId, imageId, callback) {
+        debug.log("getVote called");
+        Vote.findOne({user: userId, image: imageId}, function (err, res) {
+            callback(res);
+        });
+    },
+
+    // delete vote
+    deleteVote: function (voteId, callback) {
+        debug.log("deleteVote called");
+        Vote.remove({_id: id}, function (err, res) {
+            callback(err, res);
+        });
+    },
+
+    // check if user already voted on a picture
+    hasUserVotedImage: function (userId, imageId, callback) {
+        debug.log("hasUserVotedImage called");
+        Vote.find({user: userId, image: imageId}, function (err, res) {
+            callback(res.count > 0);
+        });
+    }
+};
