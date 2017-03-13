83436b95ba9d4b70bda055223d65f6b91ad06e44
MoritzGruber <moritzgruber@yahoo.de>
2017-03-13 17:52:00 +0100

    added frontend components

--- DockerContainer/Frontend/app/components/constants/constants.js
+++
@@ -1,5 +0,0 @@
-angular.module('fittshot.constants', [])
-
-    .constant('API_ENDPOINT', {
-        url: 'http://app.fittshot.com/api'
-    });
--- DockerContainer/Frontend/publish.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-sudo rm -r /var/www/html;
-sudo mkdir /var/www/html;
-sudo cp -r . /var/www/html;
-cd /var/www/html;
-sudo npm install && sudo bower install --allow-root;
