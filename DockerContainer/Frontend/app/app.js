14013dde8c24d3263ff0d9c768c850cef1b245ce
Christoph <cm088@hdm-stuttgart.de>
2017-04-17 21:16:55 +0200

    updated image resizing function

--- DockerContainer/Frontend/app/app.js
+++ DockerContainer/Frontend/app/app.js
@@ -90,45 +90,63 @@ fittshot
 
         $scope.inputFileChanged = function ()
         {
-            var maxWidth = 300;
-            var maxHeight = 300;
-
             var file = document.querySelector('#fileInput').files[0];
+
+            readFile(file);
+        };
+
+        function readFile(file) {
+            var reader = new FileReader();
+
+            reader.onloadend = function () {
+                processFile(reader.result, file.type);
+            };
+
+            reader.readAsDataURL(file);
+        }
+
+        function processFile(dataURL, fileType) {
+            var maxWidth = 800;
+            var maxHeight = 800;
 
             var image = new Image();
             image.src = dataURL;
 
-            var width = image.width;
-            var height = image.height;
+            image.onload = function () {
+                var width = image.width;
+                var height = image.height;
 
-            var newWidth;
-            var newHeight;
+                var newWidth;
+                var newHeight;
 
-            if (width > height) {
-                newHeight = height * (maxWidth / width);
-                newWidth = maxWidth;
-            } else {
-                newWidth = width * (maxHeight / height);
-                newHeight = maxHeight;
-            }
+                if (width > height) {
+                    newHeight = height * (maxWidth / width);
+                    newWidth = maxWidth;
+                } else {
+                    newWidth = width * (maxHeight / height);
+                    newHeight = maxHeight;
+                }
 
-            var canvas = document.createElement('canvas');
+                var canvas = document.createElement('canvas');
 
-            canvas.width = newWidth;
-            canvas.height = newHeight;
+                canvas.width = newWidth;
+                canvas.height = newHeight;
 
-            var context = canvas.getContext('2d');
+                var context = canvas.getContext('2d');
 
-            context.drawImage(this, 0, 0, newWidth, newHeight);
+                context.drawImage(this, 0, 0, newWidth, newHeight);
 
-            var dataURL = canvas.toDataURL(fileType);
+                dataURL = canvas.toDataURL(fileType);
 
-            imageService.createImage(dataURL).then(function (msg) {
-                console.log(msg);
-            }).catch(function (err) {
-                console.log(err);
-            });
-        };
+                imageService.createImage(dataURL).then(function (msg) {
+                    console.log(msg);
+                }).catch(function (err) {
+                    console.log(err);
+                });
+            };
+        }
+
+
         $rootScope.goTo('community');
 
     });
