e4cb99d30f68922998b8826b695f9c63f0e325a2
Christoph <cm088@hdm-stuttgart.de>
2017-04-17 21:58:08 +0200

    remove image resizing before upload

--- DockerContainer/Frontend/app/app.js
+++ DockerContainer/Frontend/app/app.js
@@ -91,63 +91,13 @@ fittshot
         $scope.inputFileChanged = function () {
             var file = document.querySelector('#fileInput').files[0];
 
-            readFile(file);
-        };
-
-        function readFile(file) {
-            var reader = new FileReader();
-
-            reader.onloadend = function () {
-                processFile(reader.result, file.type);
-            };
-
-            reader.readAsDataURL(file);
-        }
-
-        function processFile(dataURL, fileType) {
-            var maxWidth = 800;
-            var maxHeight = 800;
-
-            var image = new Image();
-            image.src = dataURL;
-
-            image.onload = function () {
-                var width = image.width;
-                var height = image.height;
-
-                var newWidth;
-                var newHeight;
-
-                if (width > height) {
-                    newHeight = height * (maxWidth / width);
-                    newWidth = maxWidth;
-                } else {
-                    newWidth = width * (maxHeight / height);
-                    newHeight = maxHeight;
-                }
-
-                var canvas = document.createElement('canvas');
-
-                canvas.width = newWidth;
-                canvas.height = newHeight;
-
-                var context = canvas.getContext('2d');
-
-                context.drawImage(this, 0, 0, newWidth, newHeight);
-
-                dataURL = canvas.toDataURL(fileType);
-
-                sendFile(dataURL);
-            };
-        }
-
-        function sendFile(fileData) {
-            imageService.createImage(fileData).then(function (msg) {
+            imageService.createImage(file).then(function (msg) {
                 console.log(msg);
             }).catch(function (err) {
                 console.log(err);
             });
-        }
+
+        };
 
 
         $rootScope.goTo('community');
